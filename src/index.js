2aad8a4b451bd11bc6d8b0ac70310fd5b52e47ea
vinay pandey <vinaykumar.pandey@sc.com>
2016-08-16 14:01:01 +0800

    added functionality to play against computer or bot

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import App from './components/app';
 import reducers from './reducers';
 
 let store = createStore(reducers);
-
+// bot can be passed true(to play against computer) or false to play against another player
+// by default bot is always true if not passed
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App/>
   </Provider>
   , document.querySelector('.container'));
